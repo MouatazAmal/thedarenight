f7d1574a03b5e95043df70ba47553c3599dea98f
mouatazAmal <mouataz.amal@gmail.com>
2020-02-10 17:51:01 +0100

    add getting artists feature

--- src/main/java/com/example/thedarenight/presentation/controllers/ArtistController.java
+++ src/main/java/com/example/thedarenight/presentation/controllers/ArtistController.java
@@ -3,22 +3,26 @@ package com.example.thedarenight.presentation.controllers;
 import com.example.thedarenight.presentation.enumerations.Urls;
 import com.example.thedarenight.presentation.exceptions.NullIdException;
 import com.example.thedarenight.presentation.models.dtos.ArtistDto;
+import com.example.thedarenight.presentation.models.entities.Artist;
 import com.example.thedarenight.presentation.services.database_accessers.ArtistTaskManager;
+import java.util.List;
 import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(Urls.ARTIST)
+@RestController
+@RequestMapping(Urls.ARTIST)
 public class ArtistController {
 
   @Autowired
   private ArtistTaskManager artistTaskManager;
 
-  @GetMapping("{id}")
+  @GetMapping("/{id}")
   @ResponseStatus(HttpStatus.ACCEPTED)
   public ArtistDto findArtist(@PathVariable Long id){
     if (Objects.isNull(id)){
@@ -26,6 +30,12 @@ public class ArtistController {
     }
     return artistTaskManager.findArtistById(id);
   }
+//
+//  @GetMapping
+//  @ResponseStatus(HttpStatus.ACCEPTED)
+//  public List<ArtistDto> artists(){
+//    return artistTaskManager.findAllArtists();
+//  }
 
 
 }
--- src/main/java/com/example/thedarenight/presentation/services/database_accessers/ArtistTaskManager.java
+++ src/main/java/com/example/thedarenight/presentation/services/database_accessers/ArtistTaskManager.java
@@ -5,6 +5,8 @@ import com.example.thedarenight.presentation.models.dtos.ArtistDto;
 import com.example.thedarenight.presentation.models.entities.Artist;
 import com.example.thedarenight.presentation.repositories.ArtistRepository;
 import com.example.thedarenight.presentation.services.mappers.ArtistMapper;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +24,11 @@ public class ArtistTaskManager {
   public ArtistDto findArtistById(Long id){
     Artist artist = artistRepository.findById(id).orElseThrow(()->new NotFoundArtistException(id));
     return artistMapper.fromEntityToDto(artist);
+  }
+
+  public List<ArtistDto> findAllArtists(){
+    List<ArtistDto> dtos = new ArrayList<>();
+    artistRepository.findAll().forEach(artist -> dtos.add(artistMapper.fromEntityToDto(artist)));
+    return dtos;
   }
 }
